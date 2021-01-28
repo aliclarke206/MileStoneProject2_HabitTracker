7219512544067502a358997263a4a630e6ddbc0c
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-01-28 11:58:15 +0000

    created js file and added script to create new list

---
+++ script.js
@@ -0,0 +1,36 @@
+const listsContainer = document.querySelector("[data-lists]")
+const newListForm = document.querySelector("[data-new-list-form]")
+const newListForm = document.querySelector("[data-new-list-input]")
+
+
+
+let lists = [{
+    id: 1,
+    name: "name"
+}, {
+    id: 2,
+    name: "todo"
+}]
+
+newListForm.addEventListener("submit", e => {
+    e.preventDefault()
+} )
+function render() {
+    clearElement(listsContainer)
+    lists.forEach(list =>{
+        const listElement = document.createElement("li")
+        listElement.dataset.listId = list.id
+        listElement.classList.add("list-name")
+        listElement.innerText = list.name
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+
+}
+
+render()
